ddb6ecc9bb1a9a11379a5c54e9b5c2536b455856
Marco Pfatschbacher <mpf+gh@mailq.de>
2015-09-21 23:09:03 +0200

    remove redundant version

--- sshdrill.rb
+++ sshdrill.rb
@@ -2,7 +2,6 @@ class Sshdrill < Formula
   desc "A ssh wrapper to automate tunnel creation over multiple jump hosts"
   homepage "https://github.com/mpfz0r/sshdrill"
   url "https://github.com/mpfz0r/sshdrill/archive/v0.9.tar.gz"
-  version "0.9"
   sha256 "a9f8946e5fb8ca62b2fa7e1d5aecce38579fd63f7356aa5e9100f541edd7572c"
 
   def install
