ba366e77f7e16603f20c07a290488b8a44444586
Marco Pfatschbacher <mpf+gh@mailq.de>
2015-09-29 23:43:36 +0200

    update sshdrill to 1.0

--- sshdrill.rb
+++ sshdrill.rb
@@ -1,8 +1,8 @@
 class Sshdrill < Formula
   desc "A ssh wrapper to automate tunnel creation over multiple jump hosts"
   homepage "https://github.com/mpfz0r/sshdrill"
-  url "https://github.com/mpfz0r/sshdrill/archive/v0.9.tar.gz"
-  sha256 "a9f8946e5fb8ca62b2fa7e1d5aecce38579fd63f7356aa5e9100f541edd7572c"
+  url "https://github.com/mpfz0r/sshdrill/archive/v1.0.tar.gz"
+  sha256 "c2d7ea5e9f5f16557d2eb618404127c4711164ca2d81ee3177f88a72c4841d59"
 
   def install
     system "make", "-f", "Makefile.osx", "install", "PREFIX=#{prefix}"
